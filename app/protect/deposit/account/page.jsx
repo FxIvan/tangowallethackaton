60ab17292774f9aed28fc5a5d41a19b2d123dd62
FxIvan <almendraivan210814@gmail.com>
2024-08-04 06:50:19 -0300

    alias y wallet

--- app/protect/deposit/account/page.jsx
+++ app/protect/deposit/account/page.jsx
@@ -5,7 +5,6 @@ import { NEXT_PUBLIC_BACKEND } from "constants/env";
 const getData = async () => {
   try {
     const session = await getServerSession(authOptions);
-    console.log("session", session.user.id);
 
     const response = await fetch(
       `${NEXT_PUBLIC_BACKEND}/user/info/user/${session.user.id}`,
@@ -28,7 +27,7 @@ export default async function SSRDepositAccountData() {
   const { userInfo } = await getData();
   return (
     <DepositAccountData
-      alias="aliast_feew"
+      alias={userInfo.alias || "Alias"}
       wallet={userInfo.wallet || "0x000"}
     />
   );
